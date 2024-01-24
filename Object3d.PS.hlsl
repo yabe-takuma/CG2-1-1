f402d4c0936c26eec5ba9148e00677734f904272
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-01-24 11:22:24 +0900

    CG2の4_04が完成した。

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -25,7 +25,8 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	PixelShaderOutput output;
 	float32_t4 textureColor=gTexture.Sample(gSampler,input.texcoord);
 	if(gMaterial.enableLighting!=0){
-	float cos=saturate(dot(normalize(input.normal),-gDirectionalLight.direction));
+	float NdotL=saturate(dot(normalize(input.normal),-gDirectionalLight.direction));
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 	output.color=gMaterial.color*textureColor*gDirectionalLight.color*cos*gDirectionalLight.intensity;
 	} else{
 	output.color=gMaterial.color*textureColor;
